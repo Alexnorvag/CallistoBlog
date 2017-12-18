766e6c04c0ad5e8e87def36bd9ad79c260b6a2f6
Alexnorvag <norvag@mail.ru>
2017-12-19 02:56:28 +0300

    color opacity image

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $.getJSON('js/articles.json', function (data) {
         $(data.articles).each(function (index, value) {
-            $(".col.span_2_of_3").prepend("<div class='blog-post'><a href='#' class='post-image'><img src='https://static.tumblr.com/1ee447543aa3b8a57e10af7ba60a2872/ri1vede/Jlsnz0ayt/tumblr_static_tumblr_static_2djvatz1mlgkkokw0wwsccwg8_640.jpg'></a><div class='post-details'><h3><a href='post.html' class='blog-title' rel='bookmark'>" + value.title + "</a></h3><p class='post-content'>" + value.content + "</p><h2 class='date'><i class='fa fa-calendar' aria-hidden='true'></i>14 March 2015</h2><!--<a href='#' class='post-link'>Read More...</a>--></div></div>");
+            $(".col.span_2_of_3").prepend("<div class='blog-post'><a href='#' class='post-image'><img src='https://static.tumblr.com/b1c3f552defbbd59642c44c9cecc7b78/nush1o4/q9cnof0oa/tumblr_static_tumblr_static__640.jpg'></a><div class='post-details'><h3><a href='post.html' class='blog-title' rel='bookmark'>" + value.title + "</a></h3><p class='post-content'>" + value.content + "</p><h2 class='date'><i class='fa fa-calendar' aria-hidden='true'></i>14 March 2015</h2><!--<a href='#' class='post-link'>Read More...</a>--></div></div>");
         });
     });
 });
