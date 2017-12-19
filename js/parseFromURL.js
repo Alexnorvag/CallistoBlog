0cc68dd1bf680022ec9171a4dcba3a52b72479a5
Alexnorvag <norvag@mail.ru>
2017-12-20 02:24:21 +0300

    each page have uniq content

--- js/parseFromURL.js
+++ js/parseFromURL.js
@@ -11,8 +11,10 @@ function getParameterByName(name, url) {
 var numTitle = getParameterByName('k') - 1;
 
 $.getJSON('js/articles.json', function (data) {
-    // $(".wrapper").prepend("<h3>" + data.articles[numTitle].title + "</h3>");
     $("title").prepend(data.articles[numTitle].title);
     $("a[href='']").attr("href", "" + data.articles[numTitle].image);
     $("img[src='']").attr("src", "" + data.articles[numTitle].image);
+    $(".post-title").prepend(data.articles[numTitle].title);
+    $(".post-content > p").prepend(data.articles[numTitle].content);
+    $(".header").css("background", "url("+data.articles[numTitle].image+")");
 });
