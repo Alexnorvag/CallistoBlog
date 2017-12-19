92312a049ff019ece743d896dd779a5af6cb8bbf
Alexnorvag <norvag@mail.ru>
2017-12-20 01:19:46 +0300

    min design for the inside page

--- js/parseFromURL.js
+++ js/parseFromURL.js
@@ -11,6 +11,8 @@ function getParameterByName(name, url) {
 var numTitle = getParameterByName('k') - 1;
 
 $.getJSON('js/articles.json', function (data) {
-    $(".wrapper").prepend("<h3>" + data.articles[numTitle].title + "</h3>");
+    // $(".wrapper").prepend("<h3>" + data.articles[numTitle].title + "</h3>");
     $("title").prepend(data.articles[numTitle].title);
+    $("a[href='']").attr("href", "" + data.articles[numTitle].image);
+    $("img[src='']").attr("src", "" + data.articles[numTitle].image);
 });
