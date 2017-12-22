04d56fde72ad09cce6c6083f54ef51a2b7852e1d
Alexnorvag <norvag@mail.ru>
2017-12-22 08:51:34 +0300

    added simple side menu at main page

--- js/script.js
+++ js/script.js
@@ -4,7 +4,21 @@ $(document).ready(function () {
             $(".col.span_2_of_3").prepend("<div class='blog-post'><a href='#postid' class='post-image'><img src='post-image'></a><div class='post-details'><h3><a href='post.html' class='blog-title' rel='bookmark'>" + value.title + "</a></h3><p class='post-content'>" + value.content + "</p><h2 class='date'><i class='fa fa-calendar' aria-hidden='true'></i>14 March 2015</h2><!--<a href='#' class='post-link'>Read More...</a>--></div></div>");
             $("a[href='#postid']").attr("href", "post.html?k=" + value.id);
             $("a[href='post.html']").attr("href", "post.html?k=" + value.id);
-            $("img[src='post-image']").attr("src", "" + value.image);//rmk
+            $("img[src='post-image']").attr("src", "" + value.image);
         });
+    });
+});
+
+$(document).ready(function() {
+    $("[data-toggle]").click(function() {
+        var toggle_el = $(this).data("toggle");
+        $(toggle_el).toggleClass("open-sidebar");
+    });
+});
+
+$(document).ready(function() {
+    $("[data-toggle]").click(function() {
+        var toggle_el = $(this).data("toggle");
+        $(toggle_el).toggleClass("close-sidebar");
     });
 });
